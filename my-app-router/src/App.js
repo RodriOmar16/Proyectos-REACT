1b2072dd25423ff9bf76daf8c51f64010c1ee749
Rodrigo <rodrigoomarmiranda1@gmail.com>
2024-10-26 15:55:32 -0300

    26102024_15-55

--- my-app-router/src/App.js
+++ my-app-router/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import {  BrowserRouter as Router, Routes, Route, Link, NavLink } from "react-router-dom"
+import Inicio from './components/Inicio';
+import Contacto from './components/Contacto';
+import Nosotros from './components/Nosotros';
 
 function App() {
   return (
-    <div className="App">
-      Hola mundo
-    </div>
+    <Router>
+      <div className="container">
+        <div className='btn-group mt-2'>
+          <NavLink to="/" className='btn btn-dark' activeClassName="active">
+            Inicio
+          </NavLink>
+          <NavLink to="/nosotros" className='btn btn-dark' activeClassName="active">
+            Nosotros
+          </NavLink>
+          <NavLink to="/contacto" className='btn btn-dark' activeClassName="active">
+            Contacto
+          </NavLink>
+        </div>
+        <hr/>
+        <Routes>
+          <Route path="/" element={<Inicio/>} />
+          <Route path="/contacto" element={<Contacto/>} />
+          <Route path="/nosotros" element={<Nosotros/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ my-app-router/src/components/Nosotros.jsx
@@ -0,0 +1,11 @@
+import React,{ Fragment } from 'react'
+
+const Nosotros = () => {
+  return (  
+    <Fragment>
+      <h1>Nostros</h1>
+    </Fragment>
+  );
+}
+ 
+export default Nosotros;
